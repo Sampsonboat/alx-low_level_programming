176da0fb7c1c31637d10496392aaa68a308cccd3
sampsonboat <sam23boat@gmail.com>
2023-04-04 19:55:40 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,14 +6,16 @@
  *
  * Return: resulting sum
  */
- int sum_listint(listint_t *head)
+int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint_t *temp = head
+	listint_t *temp = head;
+
 	while (temp)
 	{
 		sum += temp->n;
 		temp = temp->next;
 	}
+
 	return (sum);
 }
